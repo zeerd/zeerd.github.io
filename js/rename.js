a8a1973bfb6af08619c6432939ca072d0d4086dc
zeerd <emneg@zeerd.com>
2019-01-07 15:30:44 +0800

    mermaid

---
+++ js/rename.js
@@ -0,0 +1,11 @@
+let path = null
+
+  if (path !== window.location.pathname) {
+    path = window.location.pathname
+    document.querySelectorAll('.language-mermaid').forEach(node => {
+      node.className = "mermaid";
+    })
+    window.mermaid.initialize({ theme: 'default' })
+    window.mermaid.init(undefined, document.querySelectorAll('.language-mermaid'))
+  }
+

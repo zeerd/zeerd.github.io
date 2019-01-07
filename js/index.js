c852e93906a3801f35c8738452c4b7838083f4cf
zeerd <emneg@zeerd.com>
2019-01-07 15:53:53 +0800

    mermaid

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 let path = null
 
 setInterval(() => {
-  if (path !== window.location.pathname) {
+//  if (path !== window.location.pathname) {
     path = window.location.pathname
     window.mermaid.initialize({ theme: 'default' })
     window.mermaid.init(undefined, document.querySelectorAll('.language-mermaid'))
-  }
+//  }
 }, 1000)
 

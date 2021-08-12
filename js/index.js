26943783e239a56354f9c1ae8125eedcb38661b6
Charles Chan <zeerd@users.noreply.github.com>
2021-08-12 19:26:03 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -15,7 +15,7 @@ setInterval(() => {
     for (var i=0, len=elements.length|0; i<len; i=i+1|0) {
         encoded = encodeURI(prefix + elements[i].innerHTML);
         //elements[i].innerHTML = "<img src='" + encoded + "'>";
-        elements[i].innerHTML = "<object type='image/svg+xml' data='"
+        elements[i].innerHTML = "<object type='image/svg+xml' style='width:100%' data='"
                               + encoded + "'></object>";
         elements[i].className = "graphviz";
         elements[i].outerHTML = elements[i].outerHTML.replace(/code/g,"div");

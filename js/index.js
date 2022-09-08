97a500507d5b41f34562adc4f24cb0dde6485cee
Charles Chan <zeerd@users.noreply.github.com>
2022-09-08 13:24:41 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -10,12 +10,13 @@ setInterval(() => {
 
 setInterval(() => {
     // const prefix = "https://graphviz.zeerd.com/?";
-    const prefix = "https://g.gravizo.com/svg?";
+    const prefix = "https://www.gravizo.com/svg?";
     var elements = document.getElementsByClassName('language-graphviz');
     for (var i=0, len=elements.length|0; i<len; i=i+1|0) {
-        encoded = encodeURI(elements[i].innerHTML);
-        elements[i].innerHTML = "<img alt='DOT sample with Gravizo' src='" + prefix + encoded + "'>";
-        // elements[i].innerHTML = "<object type='image/svg+xml' style='width:100%' data='"
+        // encoded = encodeURI(elements[i].innerHTML);
+        // elements[i].innerHTML = "<img alt='DOT sample with Gravizo' src='" + prefix + encoded + "'>";
+        encoded = encodeURI(prefix + elements[i].innerHTML);
+        elements[i].innerHTML = "<object type='image/svg+xml' style='width:100%' data='"
                               + encoded + "'></object>";
         elements[i].className = "graphviz";
         elements[i].outerHTML = elements[i].outerHTML.replace(/code/g,"div");

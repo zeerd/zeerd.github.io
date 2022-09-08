bfdfe346972e2175a74801acee4bb4506a0cc59e
Charles Chan <zeerd@users.noreply.github.com>
2022-09-08 13:10:59 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -14,8 +14,8 @@ setInterval(() => {
     var elements = document.getElementsByClassName('language-graphviz');
     for (var i=0, len=elements.length|0; i<len; i=i+1|0) {
         encoded = encodeURI(prefix + elements[i].innerHTML);
-        //elements[i].innerHTML = "<img src='" + encoded + "'>";
-        elements[i].innerHTML = "<object type='image/svg+xml' style='width:100%' data='"
+        elements[i].innerHTML = "<img src='" + encoded + "'>";
+        // elements[i].innerHTML = "<object type='image/svg+xml' style='width:100%' data='"
                               + encoded + "'></object>";
         elements[i].className = "graphviz";
         elements[i].outerHTML = elements[i].outerHTML.replace(/code/g,"div");

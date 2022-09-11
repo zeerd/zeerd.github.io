d48d35a1214845d630df277a78d9a2b3d8e7cfd8
Charles Chan <zeerd@users.noreply.github.com>
2022-09-11 23:17:55 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -22,3 +22,18 @@ setInterval(() => {
         elements[i].outerHTML = elements[i].outerHTML.replace(/code/g,"div");
     }
 }, 1000)
+
+setInterval(() => {
+    // const prefix = "https://graphviz.zeerd.com/?";
+    const prefix = "https://g.gravizo.com/svg?";
+    var elements = document.getElementsByClassName('language-plantuml');
+    for (var i=0, len=elements.length|0; i<len; i=i+1|0) {
+        // encoded = encodeURI(elements[i].innerHTML);
+        // elements[i].innerHTML = "<img alt='DOT sample with Gravizo' src='" + prefix + encoded + "'>";
+        encoded = encodeURI(prefix + "@startuml\n" + elements[i].innerHTML + "\n@enduml");
+        elements[i].innerHTML = "<object type='image/svg+xml' style='width:100%' data='"
+                              + encoded + "'></object>";
+        elements[i].className = "graphviz";
+        elements[i].outerHTML = elements[i].outerHTML.replace(/code/g,"div");
+    }
+}, 1000)

817c58495fe361a293f51e3c6eff0a405bb34a43
Charles Chan <charles@zeerd.com>
2022-09-16 21:37:30 +0800

    modify plantuml

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
 let path = null
 
-// setInterval(() => {
-// //  if (path !== window.location.pathname) {
-//     path = window.location.pathname
-//     window.mermaid.initialize({ theme: 'default' })
-//     window.mermaid.init(undefined, document.querySelectorAll('.language-mermaid'))
-// //  }
-// }, 1000)
+setInterval(() => {
+//  if (path !== window.location.pathname) {
+    path = window.location.pathname
+    window.mermaid.initialize({ theme: 'default' })
+    window.mermaid.init(undefined, document.querySelectorAll('.language-mermaid'))
+//  }
+}, 1000)
 
 setInterval(() => {
     // const prefix = "https://graphviz.zeerd.com/?";
@@ -23,16 +23,16 @@ setInterval(() => {
     }
 }, 1000)
 
-// setInterval(() => {
-//     const prefix = "https://www.zeerd.com/uml.php?a=";
-//     var elements = document.getElementsByClassName('language-plantuml');
-//     for (var i=0, len=elements.length|0; i<len; i=i+1|0) {
-//         encoded = encodeURI(prefix + elements[i].innerHTML);
-//         encoded = encoded.replace(/#/g, "%23");
-//         // elements[i].innerHTML = "<img alt='DOT sample with plantuml' src='" + encoded + "'>";
-//         elements[i].innerHTML = "<object type='image/svg+xml' style='width:100%;height:100%' data='"
-//                               + encoded + "'></object>";
-//         elements[i].className = "plantuml";
-//         elements[i].outerHTML = elements[i].outerHTML.replace(/code/g,"div");
-//     }
-// }, 1000)
+setInterval(() => {
+    const prefix = "https://www.zeerd.com/plantuml.php?uml=";
+    var elements = document.getElementsByClassName('language-plantuml');
+    for (var i=0, len=elements.length|0; i<len; i=i+1|0) {
+        encoded = encodeURI(prefix + elements[i].innerHTML);
+        encoded = encoded.replace(/#/g, "%23");
+        // elements[i].innerHTML = "<img alt='DOT sample with plantuml' src='" + encoded + "'>";
+        elements[i].innerHTML = "<object type='image/svg+xml' style='width:100%;height:100%' data='"
+                              + encoded + "'></object>";
+        elements[i].className = "plantuml";
+        elements[i].outerHTML = elements[i].outerHTML.replace(/code/g,"div");
+    }
+}, 1000)

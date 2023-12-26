6799e98b6247403c8ac9a1b3439ab8a66a148177
Charles Chan <zeerd@users.noreply.github.com>
2023-12-26 17:51:34 +0800

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -21,16 +21,16 @@ setInterval(() => {
     }
 }, 1000)
 
-setInterval(() => {
-    const prefix = "https://vultr6.zeerd.com/plantuml.php?uml=";
-    var elements = document.getElementsByClassName('language-plantuml');
-    for (var i=0, len=elements.length|0; i<len; i=i+1|0) {
-        encoded = encodeURI(prefix + elements[i].innerHTML);
-        encoded = encoded.replace(/#/g, "%23");
-        // elements[i].innerHTML = "<img alt='DOT sample with plantuml' src='" + encoded + "'>";
-        elements[i].innerHTML = "<object type='image/svg+xml' style='width:100%;height:100%' data='"
-                              + encoded + "'></object>";
-        elements[i].className = "plantuml";
-        elements[i].outerHTML = elements[i].outerHTML.replace(/code/g,"div");
-    }
-}, 1000)
+// setInterval(() => {
+//     const prefix = "https://vultr6.zeerd.com/plantuml.php?uml=";
+//     var elements = document.getElementsByClassName('language-plantuml');
+//     for (var i=0, len=elements.length|0; i<len; i=i+1|0) {
+//         encoded = encodeURI(prefix + elements[i].innerHTML);
+//         encoded = encoded.replace(/#/g, "%23");
+//         // elements[i].innerHTML = "<img alt='DOT sample with plantuml' src='" + encoded + "'>";
+//         elements[i].innerHTML = "<object type='image/svg+xml' style='width:100%;height:100%' data='"
+//                               + encoded + "'></object>";
+//         elements[i].className = "plantuml";
+//         elements[i].outerHTML = elements[i].outerHTML.replace(/code/g,"div");
+//     }
+// }, 1000)
